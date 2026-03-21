eb4969b26f65bd80438421aa0d98c5d0e8868c03
sahillathwal <sahillathwal566@gmail.com>
2026-03-21 13:32:09 +0530

    adding gazebo support

--- launch/moveit_rviz.launch.py
+++ launch/moveit_rviz.launch.py
@@ -1,7 +1,19 @@
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import SetParameter
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("so101_new_calib", package_name="so101_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
+    launch_description = generate_moveit_rviz_launch(moveit_config)
+    launch_description.entities.insert(
+        0,
+        SetParameter(name="use_sim_time", value=LaunchConfiguration("use_sim_time")),
+    )
+    launch_description.entities.insert(
+        0,
+        DeclareLaunchArgument("use_sim_time", default_value="false"),
+    )
+    return launch_description
